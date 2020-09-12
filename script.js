8448024c45ea7f3c3674652af5ebebd263c5b068
Jude Clark <judeclark2019@gmail.com>
2020-09-12 11:31:11 -0400

    dynamically populate city name

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-$(function(){
+$(function () {
+  const apiKey = "593c0385215d05c9409439d0b1361f3e";
+  var cityName = "Atlanta";
 
-    const apiKey = "593c0385215d05c9409439d0b1361f3e";
-    var cityName = "Atlanta"
+  var mainQueryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=imperial&appid=" +
+    apiKey;
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid="+apiKey
+  //Current weather API Calls
+  $.ajax({
+    url: mainQueryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(mainQueryURL);
 
-    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+    $("#current-city-name").text(response.name)
 
-    console.log(queryURL)
+    console.log(response.weather[0].icon);
+    console.log("Temp in F: " + response.main.temp);
+    console.log("Humidity: " + response.main.humidity + "%");
+    console.log("Wind speed: " + response.wind.speed + " mph");
 
-})
+    var latitude = response.coord.lat;
+    console.log("Latitude: " + latitude);
+    var longitude = response.coord.lon;
+    console.log("Longitude: " + longitude);
+
+    //api call for UV
+    var UVQueryURL =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      apiKey +
+      "&lat=" +
+      latitude +
+      "&lon=" +
+      longitude;
+
+    $.ajax({
+      url: UVQueryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(UVQueryURL);
+      console.log("UV Index :" + response.value);
+    });
+  });
+
+});
