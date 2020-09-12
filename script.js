f4c4e3843058077da640c34b5d3bfe654b344d62
Jude Clark <judeclark2019@gmail.com>
2020-09-12 10:58:23 -0400

    set up API key and query URL

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(function(){
 
-    console.log("hello world")
+    const apiKey = "593c0385215d05c9409439d0b1361f3e";
+    var cityName = "Atlanta"
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid="+apiKey
+
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+    console.log(queryURL)
+
 })
