b6c9f79b1c5109690eb5ba03c31f7849ebf5cecd
Jude Clark <judeclark2019@gmail.com>
2020-09-12 13:02:51 -0400

    calling forecast data

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(function () {
   const apiKey = "593c0385215d05c9409439d0b1361f3e";
-  var cityName = "Atlanta";
+  var cityName = "Atlanta"; //TODO: make dynamic
 
-  var mainQueryURL =
+  var currentWeatherQueryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&units=imperial&appid=" +
@@ -10,26 +10,29 @@ $(function () {
 
   //Current weather API Calls
   $.ajax({
-    url: mainQueryURL,
+    url: currentWeatherQueryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(mainQueryURL);
+    // console.log(currentWeatherQueryURL);
 
-    $("#current-city-name").text(response.name)
+    $("#current-city-name").text(response.name);
     //TODO: add today's date
-    $("#current-weather-icon").attr("src", "http://openweathermap.org/img/wn/"+response.weather[0].icon+".png")
+    $("#current-weather-icon").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
+    );
 
-    $("#current-temp").text("Temperature: " + response.main.temp+" °F");
+    $("#current-temp").text("Temperature: " + response.main.temp + " °F");
 
-    $("#current-humidity").text("Humidity: " + response.main.humidity + "%")
+    $("#current-humidity").text("Humidity: " + response.main.humidity + "%");
 
-    $("#wind-speed").text("Wind speed: " + response.wind.speed + " MPH")
+    $("#wind-speed").text("Wind speed: " + response.wind.speed + " MPH");
 
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
 
-    //api call for UV
-    var UVQueryURL =
+    //current UV API Call
+    var currentUVQueryURL =
       "http://api.openweathermap.org/data/2.5/uvi?appid=" +
       apiKey +
       "&lat=" +
@@ -38,12 +41,30 @@ $(function () {
       longitude;
 
     $.ajax({
-      url: UVQueryURL,
+      url: currentUVQueryURL,
       method: "GET",
     }).then(function (response) {
-        $("#current-uv").text("UV Index: " + response.value)
-        //TODO: UV index colors
+      $("#current-uv").text("UV Index: ");
+      $("#uv-icon").text(response.value);
+      //TODO: UV index colors
+    });
+
+    //Forecast call
+    var forecastQueryURL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&exclude=current,minutely,hourly&appid=" +
+      apiKey;
+
+    console.log(forecastQueryURL);
+
+    $.ajax({
+      url: forecastQueryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response.daily);
     });
   });
-
 });
