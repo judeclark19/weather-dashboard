d91c8542f165b48043202d6c8a15628dcaf66571
Jude Clark <judeclark2019@gmail.com>
2020-09-14 22:56:45 -0400

    final push for the night

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(function () {
-  const forecastArray = ["Tomorrow", "day 2", "day 3", "day 4", "day  5"]; //might not need, this just hard code the number of days in the loops
+  const numberOfDaysToForecast = 5
   var searchHistoryArray = [];
   const apiKey = "593c0385215d05c9409439d0b1361f3e";
-  // var cityName = "Atlanta"; //TODO: make dynamic
   const inputField = $("#city-input");
   var cityName;
   var todaysDate = moment().format("D MMMM YYYY");
@@ -29,23 +28,10 @@ $(function () {
     } else {
       inputSwitch = true;
       showWeather();
-      // var includes = searchHistoryArray.includes(inputField.val());
-      // console.log(includes);
-
-      // if (includes) {
-      //   //if includes is true, show weather but do not populate
-      //   inputSwitch = true;
-      //   showWeather();
-      // } else {
-      //   //if includes is false, add input to the array
-      //   inputSwitch = true;
-      //   populateSearchBar();
-      //   showWeather();
-      // }
     }
   });
 
-  //listen to the items in the search history
+  //listen to the items in the search history sidebar
   $(document).on("click", ".list-group-item", function () {
     inputSwitch = false;
     listCity = $(this).text();
@@ -56,6 +42,7 @@ $(function () {
   function onLoad() {
     $("#search-history-items").empty();
 
+    //if there is a search history in storage, display it
     if (arrayFromStorage){
     searchHistoryArray = arrayFromStorage;}
 
@@ -64,16 +51,13 @@ $(function () {
       aSearchTerm.addClass("list-group-item");
       $("#search-history-items").prepend(aSearchTerm);
     }
-// }
-    // console.log(typeof Object.values(localStorage))
-    // console.log(JSON.parse(Object.values(localStorage)))
   }
   onLoad();
 
   function showWeather() {
     event.preventDefault();
 
-    //Get the name of the city to search based on whether the user clicked the save button or one of their previous searches
+    //retrieves a termsearch term from either the search bar or one of the previous search history items, depending on what the user clicked
     if (inputSwitch) {
       cityName = inputField.val();
     } else {
@@ -91,19 +75,18 @@ $(function () {
       apiKey;
 
     //API Calls begin
+    //=========================================================================
     $.ajax({
       url: currentWeatherQueryURL,
       method: "GET",
     }).then(function (response) {
-      //VALIDATE HERE: Check if city name is valid
-      //if response exists
-      if (response) { //if t
-
+      //VALIDATION CHECK: Check if city name is valid
+      if (response) {
         if (searchHistoryArray.includes(cityName) === false) {
           //if city name is not present in the array
           populateSearchBar();
         }
-      } else {
+      } else { //TODO: no alert appears, idk if possible
         alert("not a valid city name");
       }
 
@@ -173,7 +156,7 @@ $(function () {
         method: "GET",
       }).then(function (response) {
         //Loop to create forecast cards. See HTML file for a reference of how this looks when built. (the loops tarts on index 1 because 0 is today and we actually want to start with tomorrow)
-        for (let i = 1; i < forecastArray.length + 1; i++) {
+        for (let i = 1; i < numberOfDaysToForecast + 1; i++) {
           //create card
           var forecastCard = $("<div class='card forecast card-body'>");
 
